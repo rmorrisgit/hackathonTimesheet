0560d0a9f714b028e8a0a067188a726225a874c5
Ryan Morris <ryanmorris971@gmail.com>
2025-01-17 23:15:25 -0400

    Reverted my change. Conflicted with Doms sign in redirected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -38,23 +38,16 @@ const App = () => {
       />
       <div id="main-content">
       <Routes>
-        <Route
-          path="/"
-          element={
-            authService.isSupervisor() ? (
+      <Route
+            path="/"
+            element={
               <Main
                 filteredData={filteredData}
                 setFilteredData={setFilteredData}
                 isAuthenticated={isAuthenticated}
               />
-            ) : (
-              <div style={{ textAlign: "center", marginTop: "50px" }}>
-                <h2>Access Denied</h2>
-                <p>You do not have permission to view this data.</p>
-              </div>
-            )
-          }
-        />
+            }
+          />
           <Route path="/timesheet/:id" element={<TimesheetDetails />} />
           <Route path="/timesheet" element={<EmployeeTimesheet />} />
           <Route path="/signin" element={<SignIn setIsAuthenticated={setIsAuthenticated} />} />
